75fceeacf9cac9084d0d8926ee72cf9bc69100ff
AleixMJ <aleixtechIT@gmail.com>
2026-03-19 20:02:22 +0100

    completed the contact module

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import {home} from "./home.js";
 import {menu} from "./menu.js";
 import { about } from "./about.js";
+import { contact } from "./contact.js";
 
 const content = document.getElementById("content");
 content.appendChild(home);
