57eea5cc0592286838a7df0b219fbbca4c23a855
AleixMJ <aleixtechIT@gmail.com>
2026-03-19 11:19:06 +0100

    reworked on the design, now feels more luxury

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import "./styles.css";
 
+const content = document.getElementById("content");
+
+const home = document.createElement("div");
+home.innerHTML = `<h1>A Table for Two, A World of Spanish Passion</h1>
+ <p>In the soft embrace of candlelight and the gentle clink of crystal,
+  Casa Iberia invites you to savor Spain’s most exquisite pleasures. Our
+   seasonally inspired tasting menus tell stories of land and sea — 
+   slow-roasted suckling pig, vibrant gazpacho, and handcrafted desserts
+    kissed with olive oil and sherry — crafted with reverence and precision.
+     Each course is an invitation to slow down, to taste, to feel. Surrounded
+      by rich mahogany, original Spanish art, and a whisper of live flamenco
+       guitar, every visit becomes a private romance between you and the soul
+        of Spain.</p>`;
+
+content.appendChild(home);
+
 console.log("Hello, Webpack!");
 alert("Hello, Webpack!");
