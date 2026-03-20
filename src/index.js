d00e062722dd5a15022840da9a7e05c395200c84
AleixMJ <aleixtechIT@gmail.com>
2026-03-20 15:33:42 +0100

    added nav tabs functionality

--- src/index.js
+++ src/index.js
@@ -7,5 +7,23 @@ import { contact } from "./contact.js";
 const content = document.getElementById("content");
 content.appendChild(home);
 
+document.getElementById("nav-home").addEventListener("click", () => {
+  content.innerHTML = "";
+  content.appendChild(home);
+});
+document.getElementById("nav-menu").addEventListener("click", () => {
+  content.innerHTML = "";
+  content.appendChild(menu);
+});
+document.getElementById("nav-about").addEventListener("click", () => {
+  content.innerHTML = "";
+  content.appendChild(about);
+});
+document.getElementById("nav-contact").addEventListener("click", () => {
+  content.innerHTML = "";
+  content.appendChild(contact);
+});
+
+
 console.log("Hello, Webpack!");
 alert("Hello, Webpack!");
